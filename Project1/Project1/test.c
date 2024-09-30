9a6da2130843ea207a25acfb53d08a64b30e2791
hsdlntgm <hsdlrunning@outlook.com>
2024-09-30 10:38:18 +0800

    念念不忘必有回响！！！！！！

--- Project1/Project1/test.c
+++ Project1/Project1/test.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
 int main()
 {
-	int a = 10;
+	char arr1[] = "      初中辍笔志未穷,\n        自学编程意自雄.\n         计算机前勤探索,\n          明朝璀璨耀苍穹.";
+	printf("\n");
+	printf("\n");
+
+	printf("%s", arr1);
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("念念不忘，必有回响！！");
+	printf("\n");
+	printf("\n");
+
 	return 0;
 }
